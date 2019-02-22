44492b17995d4dd511ca1aca2383b09402381e9d
ascalmon <ascalmon@gmail.com>
2019-02-22 18:01:39 -0300

    Lyrics Route created. Cpmponent Lyrics created

---
+++ src/components/tracks/Lyrics.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+/**
+ * ComponentName
+ */
+class Lyrics extends Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div><h1>Lyrics</h1></div>
+    );
+  }
+}
+
+
+
+export default Lyrics;
