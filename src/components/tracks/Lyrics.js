d92b2cdf5f468522726e80b3629c543f830f1000
ascalmon <ascalmon@gmail.com>
2019-02-22 20:48:45 -0300

    Search component created Form input for search

--- src/components/tracks/Lyrics.js
+++ src/components/tracks/Lyrics.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import Spinner from '../layout/Spinner'
 import { Link } from 'react-router-dom';
+import Moment from 'react-moment';
 /**
  * ComponentName
  */
@@ -23,7 +24,7 @@ class Lyrics extends Component { // eslint-disable-line react/prefer-stateless-f
           `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`)
       })
       .then(res => {
-        //console.log(res.data);
+        console.log(res.data);
         this.setState({track: res.data.message.body.track})
       })
       .catch(err => console.log(err));
@@ -48,6 +49,20 @@ class Lyrics extends Component { // eslint-disable-line react/prefer-stateless-f
               <p className="card-text">{lyrics.lyrics_body}</p>
             </div>
           </div>
+          <ul className="list-group mt-3">
+            <li className="list-group-item">
+              <strong>Song Genre</strong>: {track.primary_genres.music_genre_list[0].music_genre.music_genre_name}
+            </li>
+            <li className="list-group-item">
+              <strong>Explicit Words</strong>: {' '}{track.explicit === 0 ? 'No' : 'Yes'}
+            </li>
+            <li className="list-group-item">
+              <strong>Release Date</strong>:
+                <Moment format="MM/DD/YYYY">
+                  {track.updated_time}
+                </Moment>
+            </li>
+          </ul>
         </React.Fragment>
       )
     };
