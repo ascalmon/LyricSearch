44457b74b6e05993b73d2d6046803c98f055e0d0
ascalmon <ascalmon@gmail.com>
2019-02-23 10:22:02 -0300

    Updates

---
+++ precache-manifest.4ff80d1dca67e9d68724d3bdc82549e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "79c346c0917a0c6cd4c4",
+    "url": "/LyricSearch/static/css/main.d638cc0e.chunk.css"
+  },
+  {
+    "revision": "79c346c0917a0c6cd4c4",
+    "url": "/LyricSearch/static/js/main.79c346c0.chunk.js"
+  },
+  {
+    "revision": "75db6d8163ffa9d2f40a",
+    "url": "/LyricSearch/static/js/runtime~main.75db6d81.js"
+  },
+  {
+    "revision": "0957330208057b8183fb",
+    "url": "/LyricSearch/static/js/2.09573302.chunk.js"
+  },
+  {
+    "revision": "f0d26039b9a011584d595b77a6879368",
+    "url": "/LyricSearch/static/media/spinner.f0d26039.gif"
+  },
+  {
+    "revision": "6675e17dfe3ad3b28c5de3838e3fe4d8",
+    "url": "/LyricSearch/index.html"
+  }
+];
